98a4d94a9cc70836b39e18f42427940c2f31dd96
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-04-12 22:39:50 +0530

    Adding 2 numbers

--- newproject/calc/views.py
+++ newproject/calc/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html',{'name':'sibi'})
+def add(request):
+    val1=4
+    val2=3
+    res=val1+val2
+    return render(request,'result.html',{'result':res})
