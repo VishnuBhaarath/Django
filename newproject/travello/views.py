240e3e9599088340facdd08b6ad3b1e59fb6210d
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-04-13 19:09:56 +0530

    Creating class and adding values

--- newproject/travello/views.py
+++ newproject/travello/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html",{'price':700})
+    dest1=Destination()
+    dest1.name="Hyderabad"
+    return render(request, "index.html",{'dest1': dest1})
