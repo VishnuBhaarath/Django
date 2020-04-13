9790df5a775e349229502f2c72425618dceca350
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-04-13 19:36:53 +0530

    Updation of images dynamically

--- newproject/travello/views.py
+++ newproject/travello/views.py
@@ -10,4 +10,12 @@ def index(request):
     dest1=Destination()
     dest1.name="Hyderabad"
     dest1.desc="most beautiful city"
-    return render(request, "index.html",{'dest1': dest1})
+    dest1.img='destination_1.jpg'
+
+    dest2=Destination()
+    dest2.name="Mumbai"
+    dest2.desc="most expensive city"
+    dest2.img='destination_2.jpg'
+
+    dests = [dest1, dest2]
+    return render(request, "index.html",{'dests': dests})
