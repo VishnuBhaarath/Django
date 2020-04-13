e305cfa856e364d5e6fd29696cdc7d25a14dffd0
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-04-13 10:45:39 +0530

    Adding static files

--- newproject/newproject/urls.py
+++ newproject/newproject/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('',include('calc.urls')),
-    path('admin/', admin.site.urls),
+    path('',include('travello.urls'))
 ]
