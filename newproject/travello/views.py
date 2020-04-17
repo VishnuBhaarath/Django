6a89432d4b2a2f1ecd11b3d39cc7bac47d67822a
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-04-17 11:31:43 +0530

    save and fetching data from database

--- newproject/travello/views.py
+++ newproject/travello/views.py
@@ -7,15 +7,5 @@ from .models import Destination
 
 # Create your views here.
 def index(request):
-    dest1=Destination()
-    dest1.name="Hyderabad"
-    dest1.desc="most beautiful city"
-    dest1.img='destination_1.jpg'
-
-    dest2=Destination()
-    dest2.name="Mumbai"
-    dest2.desc="most expensive city"
-    dest2.img='destination_2.jpg'
-
-    dests = [dest1, dest2]
+    dests= Destination.objects.all()
     return render(request, "index.html",{'dests': dests})
