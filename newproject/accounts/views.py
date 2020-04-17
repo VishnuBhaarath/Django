149f1124550b85b38d98d348bfae680aa8de60b3
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-04-17 17:42:40 +0530

    User registration

--- newproject/accounts/views.py
+++ newproject/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def register(request):
+    return render(request, 'register.html')
 
 # Create your views here.
