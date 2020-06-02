29ea82905ed2de7727424ab38f69e406a5fce6a7
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-06-02 18:05:19 +0530

    Polls app

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'travello.apps.TravelloConfig',
+    
 
     'django.contrib.admin',
     'django.contrib.auth',
--- newproject/newproject/urls.py
+++ newproject/newproject/urls.py
@@ -19,9 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',include('travello.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.urls'))
 ]
 
-urlpatterns= urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
