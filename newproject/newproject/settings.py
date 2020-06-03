d32d8a038f5d8b36141750e6b1e06bdc13979bfb
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-06-03 16:04:01 +0530

    Sqlite as backend

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -77,11 +77,9 @@ WSGI_APPLICATION = 'newproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-        'USER': 'postgres',
-        'PASSWORD':'1234',
-        'HOST':'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      
     }
 }
 
