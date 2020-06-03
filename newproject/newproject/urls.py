f8fd8663b8528c3dc9ca1dbbbfc6deb2db6c4263
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-06-03 16:56:11 +0530

    Add

--- newproject/newproject/urls.py
+++ newproject/newproject/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',include('polls.urls')),
+    path('',include('travello.urls')),
     path('admin/', admin.site.urls),
     
 ]
