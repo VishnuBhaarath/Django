c422975f5c85419b02a2eecf4847400dd1589270
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-06-04 15:54:12 +0530

    Detailview

--- newproject/polls/views.py
+++ newproject/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Question
 from django.http import HttpResponse
 
@@ -9,7 +9,8 @@ def index(request):
     return render(request, 'polls/pollsindex.html', context)
     
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/pollsdetail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
