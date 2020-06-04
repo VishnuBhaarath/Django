07602352b693dd31fa6be3f5f5881ec9d27b538b
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-06-04 15:22:31 +0530

    templates

--- newproject/polls/urls.py
+++ newproject/polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
--- newproject/polls/views.py
+++ newproject/polls/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from .models import Question
 from django.http import HttpResponse
 
 
-def home(request):
-     return HttpResponse("Hello, world. You're at the polls index.")
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/pollsindex.html', context)
+    
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
